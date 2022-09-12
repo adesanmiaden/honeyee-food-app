c9c7f5ee5a89fb9b6ab37c3d27caefeb892f8719
Adesanmi Adeniyi <adesanmi.adeniyi@tellerium.io>
2022-09-11 23:07:43 -0700

    heaader

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import './Header.css';
+import HeaderCartButton from './HeaderCartButton';
 
 function Header() {
   return (
     <>
     <header className='header'>
         <h1>Honeyee</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
     </header>
     <div className='main-image'>
         <img src={mealsImage} alt=''/>
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CartIcon from '../Cart/CartIcon';
+import './HeaderCartButton.css'
+
+function HeaderCartButton(props) {
+  return (
+    <button className='button'>
+        <span>
+            <CartIcon/>
+        </span>
+        <span>Your Cart</span>
+        <span className='badge'>3</span>
+    </button>
+  )
+}
+
+export default HeaderCartButton
