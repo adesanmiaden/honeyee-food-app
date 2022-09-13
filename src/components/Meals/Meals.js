b86b0a0421afa117270c8a2e78b5f8d3e346fddf
Adesanmi Adeniyi <adesanmi.adeniyi@tellerium.io>
2022-09-12 21:51:16 -0700

    meals

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
 
 function Meals() {
   return (
-    <div>Meals</div>
+    <>
+    <MealsSummary/>
+    <AvailableMeals/>
+    </>
   )
 }
 
