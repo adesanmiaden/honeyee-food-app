8c6f08fa52869c74c06374132311ba7c9d5ad6c6
Adesanmi Adeniyi <adesanmi.adeniyi@tellerium.io>
2022-10-03 22:05:27 -0700

    firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,47 +1,91 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import './AvailableMeals.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-    {
-      id: 'm5',
-      name: 'Swedish pizza',
-      description: 'Tasty and delicious',
-      price: 15.99,
-    },
-  ];
+// const DUMMY_MEALS = [
+//     {
+//       id: 'm1',
+//       name: 'Sushi',
+//       description: 'Finest fish and veggies',
+//       price: 22.99,
+//     },
+//     {
+//       id: 'm2',
+//       name: 'Schnitzel',
+//       description: 'A german specialty!',
+//       price: 16.5,
+//     },
+//     {
+//       id: 'm3',
+//       name: 'Barbecue Burger',
+//       description: 'American, raw, meaty',
+//       price: 12.99,
+//     },
+//     {
+//       id: 'm4',
+//       name: 'Green Bowl',
+//       description: 'Healthy...and green...',
+//       price: 18.99,
+//     },
+//     {
+//       id: 'm5',
+//       name: 'Swedish pizza',
+//       description: 'Tasty and delicious',
+//       price: 15.99,
+//     },
+//   ];
 
 function AvailableMeals() {
+  const[meals, setMeals] = useState([]);
+
+  // const fetchMeals = async() => {
+  //   try{
+  //     const response = await fetch ('https://food-order-app-dab3c-default-rtdb.firebaseio.com/');
+  //     const data = await response.json();
+  //     console.log('dataaaa', data);
+  //     const fetchedMeals = [];
+
+  //     for(const key in data){
+  //       const Meals ={
+  //         id: key,
+  //         ...data[key]
+  //       }
+
+  //       console.log(Meals);
+
+  //       fetchedMeals.push(Meals)
+  //     }
+
+  //     setMeals(fetchedMeals);
+  //   } catch (err){
+      
+  //   }
+  //   };
+
+    useEffect=(async()=> {
+      const response = await fetch ('https://food-order-app-dab3c-default-rtdb.firebaseio.com/meals.json');
+      const data = await response.json();
+      console.log('dataaaa', data);
+      const fetchedMeals = [];
+
+      for(const key in data){
+        const Meals ={
+          id: key,
+          ...data[key]
+        }
+
+        fetchedMeals.push(Meals)
+      }
+
+      setMeals(fetchedMeals);
+    }, [])
+
   return (
     <section className='meals'>
         <Card>
         <ul>
-            {DUMMY_MEALS.map(meal =>
+            {meals.map(meal =>
                 <MealItem 
                 key={meal.id}
                 id={meal.id}
